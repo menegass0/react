a406ba05c993557f142ce2381578e365b4e4545f
menegass0 <victormenegasso@unisantos.br>
2023-02-15 14:11:52 -0300

    creating-context-and-ussing-useReducer

--- quiz/src/components/Welcome.jsx
+++ quiz/src/components/Welcome.jsx
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
 import Quiz from '../img/quiz.svg';
 
 import "./Welcome.css";
 
 const welcome = () => {
-  return (
-    <div id='welcome'>
-        <h2>Seja bem-vindo</h2>
-        <p>clique no botão para começar</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Inicio do Quiz" />
-    </div>
-  )
+    const [quizState, dispatch] = useContext(QuizContext);
+    return (
+        <div id='welcome'>
+            <h2>Seja bem-vindo</h2>
+            <p>clique no botão para começar</p>
+            <button onClick={() => dispatch({type: "CHANGE_STATE"})}>
+                Iniciar
+            </button>
+            <img src={Quiz} alt="Inicio do Quiz" />
+        </div>
+    )
 }
 
 export default welcome
