0a833e8e5b58352b6289f30c016e2b11e7801a10
menegass0 <victormenegasso@unisantos.br>
2023-02-22 21:33:06 -0300

    adding-and-removing-from-favorites-using-context

--- react course/lesson-2/src/store/favorites-context.js
+++ react course/lesson-2/src/store/favorites-context.js
@@ -3,10 +3,13 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext({
     favorites: {},
     totalFavorites: 0,
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {},
 });
 
-function FavoritesContextProvider(props) {
-    const [userFavorites, setUserFavorites] = useState();
+export function FavoritesContextProvider(props) {
+    const [userFavorites, setUserFavorites] = useState([]);
     
     function addFavoriteHandler(favoriteMeetup) {
         setUserFavorites((prevUserFavorites) => {
@@ -16,21 +19,27 @@ function FavoritesContextProvider(props) {
 
     function removeFavoriteHandler(meetupId) {
         setUserFavorites(prevUserFavorites => {
-            return prevUserFavorites.filter(meetup => meetup !== meetupId);
+            return prevUserFavorites.filter(meetup => meetup.id !== meetupId);
         });
     }
 
     function itemIsFavoriteHandler(meetupId) {
-        return userFavorites.some(meetup => meetup === meetupId);
+        return userFavorites.some(meetup => meetup.id === meetupId);
     }
 
     const context = {
         favorites: userFavorites,
         totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler,
     };
 
     
-    return <FavoritesContext.Provider value={context}>
+    return (<FavoritesContext.Provider value={context}>
         {props.children}
     </FavoritesContext.Provider>
+    );
 }
+
+export default FavoritesContext;
